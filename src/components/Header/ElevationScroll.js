9feb7a96ce2c59d7fd0a6627eefabe03417adc36
Petar Panayotov <dakona98@gmail.com>
2023-03-15 20:51:30 +0200

    Create ElevationScroll.js

---
+++ src/components/Header/ElevationScroll.js
@@ -0,0 +1,14 @@
+import { useScrollTrigger } from "@mui/material";
+import React from "react";
+
+export function ElevationScroll(props) {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
