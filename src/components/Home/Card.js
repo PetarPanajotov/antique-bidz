914a0a6c155f2591ff7624f0420bb8daa40b7a54
Petar Panayotov <dakona98@gmail.com>
2023-03-16 18:12:00 +0200

    Added cards with lastest offers.

---
+++ src/components/Home/Card.js
@@ -0,0 +1,27 @@
+import { Box, Button, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import styles from './Card.module.css'
+
+export function Card() {
+    return (
+        <Grid item xs={4}>
+            <Paper elevation={3}>
+                <img
+                    src='https://images.thdstatic.com/productImages/5669f4a1-b700-4122-bc2e-881bccecd02a/svn/black-wood-yosemite-home-decor-wall-clocks-clkb2a147-64_1000.jpg'
+                    alt='some'
+                    className={styles['card-image']}
+                ></img>
+                <Box paddingTop={1}>
+                    <Typography variant="h6">Old CLock</Typography>
+                </Box>
+                <Box paddingTop={1}>
+                    <Typography variant="subtitle1">Category: clock</Typography>
+                </Box>
+                <Box paddingTop={3} height = {80}>
+                    <Button variant="contained">Details</Button>
+                </Box>
+            </Paper>
+        </Grid>
+    )
+}
