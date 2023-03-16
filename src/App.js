94bd2f304713c851271bf36bd826bf99b4fe7f21
Petar Panayotov <dakona98@gmail.com>
2023-03-16 19:21:50 +0200

    Added login page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Container } from "@mui/system";
 import { Header } from "./components/Header/Header"; import { Home } from "./components/Home/Home";
-;
+import { Login } from "./components/Login/Login";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+        {/* <Home /> */}
+        <Login></Login>
     </>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,33 @@
+import { LockOutlined } from "@mui/icons-material";
+import { Avatar, Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+
+export function Login() {
+    return (
+        <form>
+            <Container>
+                <Grid paddingTop={'25vh'} paddingBottom={'25vh'} sx={{ textAlign: 'center', display: 'flex', justifyContent: 'center' }}>
+                    <Paper elevation={11} sx={{ backgroundColor: 'none', flexDirection: 'column', width: '400px' }}>
+                        <Box paddingTop={3}>
+                            <Box sx = {{display: 'flex', justifyContent: 'center'}}>
+                                <Avatar sx={{ m: 1, bgcolor: 'purple' }}>
+                                    <LockOutlined></LockOutlined>
+                                </Avatar>
+                            </Box>
+                            <Typography variant="h3">Sign in</Typography>
+                        </Box>
+                        <Box paddingTop={5}>
+                            <TextField id="filled-basic" label="Email" variant="outlined" />
+                        </Box>
+                        <Box paddingTop={2}>
+                            <TextField id="filled-basic" label="Password" variant="outlined" />
+                        </Box>
+                        <Box padding={5}>
+                            <Button variant="contained">Sign in</Button>
+                        </Box>
+                    </Paper>
+                </Grid >
+            </Container>
+        </form>
+    );
+};
