4202d34e8bf22bbe09cf5a0ed24e5b4cc7920ac7
Petar Panayotov <dakona98@gmail.com>
2023-03-16 19:45:51 +0200

    Removed unnecessary imports.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Container } from "@mui/system";
-import { Header } from "./components/Header/Header"; import { Home } from "./components/Home/Home";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
 
