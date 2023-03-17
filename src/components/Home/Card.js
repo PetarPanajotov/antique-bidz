5d7039e96ca35ffab333d34dfc1e308c43716ab3
Petar Panayotov <dakona98@gmail.com>
2023-03-17 23:24:09 +0200

    Separated css from js file.

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -6,20 +6,20 @@ import styles from './Card.module.css'
 export function Card() {
     return (
         <Grid item xs={4} >
-            <Paper elevation={3} className={styles['card-wrapper']}>
+            <Paper elevation= {3} className= {styles['card-wrapper']}>
                 <img
                     src='https://images.thdstatic.com/productImages/5669f4a1-b700-4122-bc2e-881bccecd02a/svn/black-wood-yosemite-home-decor-wall-clocks-clkb2a147-64_1000.jpg'
                     alt='some'
                     className={styles['card-image']}
                 ></img>
-                <Box paddingTop={1}>
+                <Box paddingTop= {1}>
                     <Typography variant="h6">Old CLock</Typography>
                 </Box>
-                <Box paddingTop={1}>
+                <Box paddingTop= {1}>
                     <Typography variant="subtitle1">Category: clock</Typography>
                 </Box>
-                <Box paddingTop={3} className={styles['card-button']}>
-                    <Button variant="contained">Details</Button>
+                <Box paddingTop= {3} paddingBottom= {4}>
+                    <Button variant="contained" className={styles['card-button']}>Details</Button>
                 </Box>
             </Paper>
         </Grid>
