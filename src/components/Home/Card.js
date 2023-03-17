f65842c1cdd52d3aceb5970392ef35a23bacd466
Petar Panayotov <dakona98@gmail.com>
2023-03-17 23:04:33 +0200

    Separated css from js file.

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, StyledEngineProvider, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import styles from './Card.module.css'
@@ -6,7 +6,7 @@ import styles from './Card.module.css'
 export function Card() {
     return (
         <Grid item xs={4} >
-            <Paper elevation={3} sx= {{marginBottom: 5 }}>
+            <Paper elevation={3} className={styles['card-wrapper']}>
                 <img
                     src='https://images.thdstatic.com/productImages/5669f4a1-b700-4122-bc2e-881bccecd02a/svn/black-wood-yosemite-home-decor-wall-clocks-clkb2a147-64_1000.jpg'
                     alt='some'
@@ -18,7 +18,7 @@ export function Card() {
                 <Box paddingTop={1}>
                     <Typography variant="subtitle1">Category: clock</Typography>
                 </Box>
-                <Box paddingTop={3} height = {80}>
+                <Box paddingTop={3} className={styles['card-button']}>
                     <Button variant="contained">Details</Button>
                 </Box>
             </Paper>
