fdcbf6c895a09b2ab17b7ae8866eb52764453515
Petar Panayotov <dakona98@gmail.com>
2023-03-27 13:50:59 +0300

    Navigation updates depends if there is user.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,31 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 import { ElevationScroll } from "./ElevationScroll";
 import styles from "./Header.module.css"
 
 export function Header() {
+    const { user, logout } = useContext(AuthContext);
     return (
         <ElevationScroll>
             <AppBar className={styles['container']}>
                 <Toolbar disableGutters>
                     <Typography sx={{ textDecoration: 'underline' }}>AntiqueBidz</Typography>
                     <Box className={styles['nav-links-wrapper']}>
-                        <Link to={"/home"}><Typography className={styles["nav-links"]}>Home</Typography></Link>
+                        <Link to={"/"}><Typography className={styles["nav-links"]}>Home</Typography></Link>
                         <Link to={"/catalogue"}><Typography className={styles["nav-links"]}>Catalogue</Typography></Link>
-                        <Link to={"/create"}><Typography className={styles["nav-links"]}>Create Bid</Typography></Link>
-                        <Link to={"/login"}><Typography className={styles["nav-links"]}>Login</Typography></Link>
-                        <Link to={"/register"}><Typography className={styles["nav-links"]}>Register</Typography></Link>
-                        <Link to={"/logout"}><Typography className={styles["nav-links"]}>Logout</Typography></Link>
+                        {user ?
+                            <Link to={"/create"}><Typography className={styles["nav-links"]}>Create Bid</Typography></Link>
+                            :
+                            <>
+                                <Link to={"/login"}><Typography className={styles["nav-links"]}>Login</Typography></Link>
+                                <Link to={"/register"}><Typography className={styles["nav-links"]}>Register</Typography></Link>
+                            </>
+                        }
+                        {user &&
+                            <Link to={"javascript:void(0)"}><Typography className={styles["nav-links"]} onClick={logout}>Logout</Typography></Link>
+                        }
                         <Link to={"/about"}><Typography className={styles["nav-links"]}>About Us</Typography></Link>
                     </Box>
                 </Toolbar>
