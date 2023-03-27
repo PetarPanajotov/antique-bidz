42caa819f54982a15fddadcc71b31f82e79dd7f6
Petar Panayotov <dakona98@gmail.com>
2023-03-27 18:16:40 +0300

    Created useRemainingTime hook.

---
+++ src/hooks/useRemainingTime.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function useRemainingTime(initialDate) {
+    const [remainingTime, setRemainingTime] = useState(initialDate);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            let [hours, minutes, seconds] = remainingTime;
+            if (seconds === 0) {
+                minutes--;
+                seconds = 60;
+            }
+            if (minutes === 0) {
+                hours--;
+                minutes = 59;
+            }
+            seconds--;
+            setRemainingTime([hours, minutes, seconds]);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [remainingTime]);
+
+    const formattedTime = `${remainingTime[0].toString().padStart(2, "0")}:${remainingTime[1].toString().padStart(2, "0")}:${remainingTime[2].toString().padStart(2, "0")}`;
+
+    return {formattedTime};
+};
--- src/services/antiqueService.js
+++ src/services/antiqueService.js
@@ -3,3 +3,6 @@ import { GET } from "./requester";
 export function getAll() {
     return GET('/data/antiques');
 };
+export function getOne(id) {
+    return GET(`/data/antiques/${id}`);
+};
