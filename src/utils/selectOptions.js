b1eba3b06153a4fc29b97bdf663bcffc6d044bb7
Petar Panayotov <dakona98@gmail.com>
2023-03-29 17:35:42 +0300

    Added categories and duration options.

---
+++ src/utils/selectOptions.js
@@ -0,0 +1,11 @@
+export const categoriesOptions = {
+    'Furnitures': ['Chairs', 'Tables', 'Cabinets', 'Desks', 'Beds', 'Chests'],
+    'Decorative Arts': ['Ceramics and Pottery', 'Glassware', 'Metalware', 'Textiles', 'Folk Art'],
+    'Fine Arts': ['Paintings', 'Sculptures', 'Drawnings and Prints', 'Photography'],
+    'Jewelry and Watches': ['Rings', 'Necklaces', 'Earrings', 'Bracelets', 'Pocket Watches'],
+    'Collectibles': ['Coins and Stamps', 'Sports Memorabilia', 'Toys and Games', 'Advertising Memorabilia', 'Political Memorabilia'],
+    'Lightnings': ['Chandeliers', 'Lamps', 'Candlesticks'],
+    'Musical Instruments': ['String Instruments', 'Wind Instruments', 'Percussion Instruments'],
+    'Books and Manuscripts': ['Rare Books', 'Historical Documents', 'Maps and Atlases', 'Autographs and Signatures', 'Manuscripts']
+}
+export const durationOptions = [24, 48, 72, 168]
