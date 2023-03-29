953a0a3d091b16b10333cc7a1f2491ce819b2cf2
Petar Panayotov <dakona98@gmail.com>
2023-03-29 19:06:37 +0300

    Fixed link styles.

--- src/components/Catalogue/CardCatalogue.js
+++ src/components/Catalogue/CardCatalogue.js
@@ -1,27 +1,29 @@
-import { Grid, Paper, Typography } from "@mui/material"
+import { Grid, Paper, Typography, Box } from "@mui/material"
 import { Link } from "react-router-dom";
 import { useRemainingTime } from "../../hooks/useRemainingTime";
 import { dateConvert } from "../../utils/dateUtil";
 import styles from './CardCatalogue.module.css';
 
 export function CardCatalogue({ antique }) {
-    const {formattedTime} = useRemainingTime(dateConvert(antique.bidDetails.endDate));
+    const { formattedTime } = useRemainingTime(dateConvert(antique.bidDetails.endDate));
 
     return (
         <Grid item xs={12} md={6} lg={3} >
-            <Link to={`/catalogue/details/${antique._id}`} style={{ textDecoration: "none" }}>
-                <Paper className={styles['card-wrapper']}>
-                    <img
-                        src={antique.imgURL}
-                        alt='some'
-                        className={styles['card-image']}
-                    ></img>
-                    <Typography variant="h6">{antique.antiqueName}</Typography>
-                    <Typography variant="subtitle1">Current Bid</Typography>
-                    <Typography variant="h5">${antique.bidDetails.startBid}</Typography>
-                    <Typography paddingTop={2}>End in: {formattedTime}</Typography>
-                </Paper>
-            </Link>
-        </Grid>
+            <Paper className={styles['card-wrapper']}>
+                <Link to={`/catalogue/details/${antique._id}`} style={{ textDecoration: 'inherit', color: 'inherit' }}>
+                    <Box>
+                        <img
+                            src={antique.imgURL}
+                            alt='some'
+                            className={styles['card-image']}
+                        ></img>
+                        <Typography variant="h6">{antique.antiqueName}</Typography>
+                        <Typography variant="subtitle1">Current Bid</Typography>
+                        <Typography variant="h5">${antique.bidDetails.startBid}</Typography>
+                        <Typography paddingTop={2}>End in: {formattedTime}</Typography>
+                    </Box>
+                </Link>
+            </Paper>
+        </Grid >
     );
 };
