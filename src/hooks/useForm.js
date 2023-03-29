8cf18bb5f37c1acd5c26ab02cc72f4c64e1e5a93
Petar Panayotov <dakona98@gmail.com>
2023-03-29 17:35:25 +0300

    Added specific case for this hook.

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
 export function useForm(initialValues) {
-    const [formValues, setFormValues] = useState(initialValues)
+    const [formValues, setFormValues] = useState(initialValues);
 
     const onChange = (e) => {
+        //this is specific case for nested object upon CreateBid.
+        if (e.target.name ===  'startBid' || e.target.name === 'endDate') {
+            return setFormValues(state => ({ ...state, bidDetails: { ...state.bidDetails, [e.target.name]: e.target.value } }))
+        }
         setFormValues(state => ({ ...state, [e.target.name]: e.target.value }))
     };
     return {
