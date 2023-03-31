a6c93f81aa90f5830b24e5c49b437f2ebab77223
Petar Panayotov <dakona98@gmail.com>
2023-03-31 15:53:50 +0300

    Added fetch collecting size, sorting, pagination

--- src/services/antiqueService.js
+++ src/services/antiqueService.js
@@ -1,8 +1,14 @@
 import { GET, POST } from "./requester";
 
-export function getAll() {
-    return GET('/data/antiques');
+export function getAll(offset) {
+    const pageSize = 8;
+    const query = encodeURI(`offset=${offset}&pageSize=${pageSize}`);
+    const sortQuery = encodeURI(`?sortBy=_createdOn desc`);
+    return GET(`/data/antiques${sortQuery}&${query}`);
 };
+export function getCollectionSize() {
+    return GET(`/data/antiques?count`)
+}
 export function getOne(id) {
     return GET(`/data/antiques/${id}`);
 };
