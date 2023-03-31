5ba185e036858cf5c863077ca48b9c84edebc66c
Petar Panayotov <dakona98@gmail.com>
2023-03-31 18:56:58 +0300

    implement home page.

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,27 +1,37 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import styles from './Card.module.css'
+import { useRemainingTime } from "../../hooks/useRemainingTime";
+import { dateConvert } from "../../utils/dateUtil";
+import { Link } from "react-router-dom";
 
-export function Card() {
+export function Card({antique}) {
+    const { formattedTime } = useRemainingTime(dateConvert(antique.bidDetails.endDate));
+
     return (
-        <Grid item xs={4} >
-            <Paper elevation= {3} className= {styles['card-wrapper']}>
-                <img
-                    src='https://images.thdstatic.com/productImages/5669f4a1-b700-4122-bc2e-881bccecd02a/svn/black-wood-yosemite-home-decor-wall-clocks-clkb2a147-64_1000.jpg'
-                    alt='some'
-                    className={styles['card-image']}
-                ></img>
-                <Box paddingTop= {1}>
-                    <Typography variant="h6">Old CLock</Typography>
-                </Box>
-                <Box paddingTop= {1}>
-                    <Typography variant="subtitle1">Category: clock</Typography>
-                </Box>
-                <Box paddingTop= {3} paddingBottom= {4}>
-                    <Button variant="contained" className={styles['card-button']}>Details</Button>
-                </Box>
+        <Grid item xs={12} md={6} lg={4} >
+            <Paper className={styles['card-wrapper']}>
+                <Link to={`/catalogue/details/${antique._id}`} style={{ textDecoration: 'inherit', color: 'inherit' }}>
+                    <Box>
+                        <Box>
+                            <img
+                                src={antique.imgURL}
+                                alt='some'
+                                className={styles['card-image']}
+                            ></img>
+                        </Box>
+                        <Box className={styles['card-name']}>
+                            <Typography variant="h6">{antique.antiqueName}</Typography>
+                        </Box>
+                        <Box>
+                            <Typography variant="subtitle1">Current Bid</Typography>
+                            <Typography variant="h5">${antique.bidDetails.startBid}</Typography>
+                            <Typography paddingTop={2}>End in: {formattedTime}</Typography>
+                        </Box>
+                    </Box>
+                </Link>
             </Paper>
-        </Grid>
+        </Grid >
     )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,43 @@
 import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import { Card } from "./Card";
-import styles from './Home.module.css'
+import styles from './Home.module.css';
+import { useContext } from "react";
+import { AntiqueContext } from "../../contexts/AntiqueContext";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export function Home() {
+    const { antiqueData } = useContext(AntiqueContext);
+    const { auth } = useContext(AuthContext);
+    const lastThree = antiqueData.slice(0, 3);
+
     return (
-            <Container maxWidth="lg" className={styles['home-container']}>
-                <Box className={styles['text-wrapper']}>
-                    <Box paddingTop= {10}>
-                        <Typography variant="h1">
-                            Sell Your Antiques with Confidence
-                        </Typography>
-                    </Box>
-                    <Box paddingTop= {10}>
-                        <Typography variant="h4">
-                            Looking to sell a valuable family heirloom or unique find? Look no further. Our expert team of antique appraisers and bidders is here to help you get the best price for your treasured item. With years of experience in the industry, we know how to navigate the bidding process and get you the highest possible return on your investment.
-                        </Typography>
-                    </Box>
-                    <Box paddingTop= {4}>
+        <Container maxWidth="lg" className={styles['home-container']}>
+            <Box className={styles['text-wrapper']}>
+                <Box paddingTop={10}>
+                    <Typography variant="h1">
+                        Sell Your Antiques with Confidence
+                    </Typography>
+                </Box>
+                <Box paddingTop={10}>
+                    <Typography variant="h4">
+                        Looking to sell a valuable family heirloom or unique find? Look no further. Our expert team of antique appraisers and bidders is here to help you get the best price for your treasured item. With years of experience in the industry, we know how to navigate the bidding process and get you the highest possible return on your investment.
+                    </Typography>
+                </Box>
+                <Box paddingTop={4}>
+                    <Link to={auth.accessToken ? '/catalogue' : '/login'} style={{ textDecoration: 'inherit', color: 'inherit' }}>
                         <Button variant="contained" className={styles['home-button']}>
                             <Typography variant="h6">Get Started </Typography>
                         </Button>
-                    </Box>
+                    </Link>
                 </Box>
-                <Box paddingTop={10} className={styles['card-wrapper']}>
-                    <Typography variant="h3">Last Offers:</Typography>
-                    <Grid container spacing= {8} paddingTop= {4} paddingBottom= {5}>
-                        <Card />
-                        <Card />
-                        <Card />
-                    </Grid>
-                </Box>
-            </Container>
+            </Box>
+            <Box paddingTop={10} className={styles['card-wrapper']}>
+                <Typography variant="h3">Last Offers:</Typography>
+                <Grid container spacing={8} paddingTop={4} paddingBottom={5}>
+                    {lastThree.map(antique => <Card key={antique._id} antique={antique} />)}
+                </Grid>
+            </Box>
+        </Container >
     );
 };
