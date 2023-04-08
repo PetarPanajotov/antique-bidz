e8aa9a6fed38e297c3c7e6ea57c128487bdeac76
Petar Panayotov <dakona98@gmail.com>
2023-04-08 15:59:33 +0300

    More route guards

--- src/components/common/GuestRouteGuard.js
+++ src/components/common/GuestRouteGuard.js
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-import { Navigate, Outlet} from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-export function UserRouteGuard() {
+export function GuestRouteGuard() {
     const { user } = useContext(AuthContext);
-    if (user) {
-        return <Navigate to='/' />
+    if (!user) {
+        return <Navigate to='/login' />
     };
 
     return <Outlet />
